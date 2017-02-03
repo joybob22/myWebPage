9422dca7fc04caa0b7d5805fb3ff69b656c3f957
joybob22 <braydenlemke8@gmail.com>
2017-02-02 18:35:17 -0700

    added resume stuff

--- resume.js
+++ resume.js
@@ -1,3 +1,24 @@
 /**
  * Created by braydenlemke on 12/7/16.
  */
+
+$(".educationClick").on("click", function() {
+    $("#arrow1").toggleClass("hideContentRotate");
+    $("#arrow1").toggleClass("showContentRotate");
+    $("#shadow1").toggle();
+    $(".educationContent").slideToggle();
+});
+
+$("#workClick").on("click", function() {
+    $("#arrow2").toggleClass("hideContentRotate");
+    $("#arrow2").toggleClass("showContentRotate");
+    $("#shadow2").toggle();
+    $("#workContent").slideToggle();
+});
+
+$("#summaryClick").on("click", function() {
+    $("#arrow3").toggleClass("hideContentRotate");
+    $("#arrow3").toggleClass("showContentRotate");
+    $("#shadow3").toggle();
+    $("#summaryContent").slideToggle();
+});
